b40e248439967e81ffebe8ddaee472bd0425629a
ieb <1@wa.vg>
2022-04-10 16:20:59 +0800

    added letsencrypt and self-signed certs

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.17
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921
 )
 
-require golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
+require (
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20210616045830-e2b7044e8c71 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,19 +1,109 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"flag"
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/pkg/browser"
+	"golang.org/x/crypto/acme/autocert"
 	"log"
+	"math/big"
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"regexp"
 	"strings"
+	"sync"
+	"time"
 )
 
+var builtInSSL = false
+var selfSSL = false
+var port = 13331
+
+const RSABits = 2048
+const ValidFor = time.Hour * 1440
+
+var certCache sync.Map
+
+func GetSelfSignedCertificate(clientHello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+	if cert, ok := certCache.Load(clientHello.ServerName); ok {
+		return cert.(*tls.Certificate), nil
+	}
+	priv, err := rsa.GenerateKey(rand.Reader, RSABits)
+	if err != nil {
+		return nil, err
+	}
+
+	notBefore := time.Now()
+	notAfter := notBefore.Add(ValidFor)
+
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		return nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"Acme Co"},
+		},
+		NotBefore:             notBefore,
+		NotAfter:              notAfter,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	host := clientHello.ServerName
+	if ip := net.ParseIP(host); ip != nil {
+		template.IPAddresses = append(template.IPAddresses, ip)
+	} else {
+		template.DNSNames = append(template.DNSNames, host)
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	if err != nil {
+		return nil, err
+	}
+
+	cert := &tls.Certificate{
+		Certificate: [][]byte{derBytes},
+		PrivateKey:  priv,
+	}
+	certCache.Store(clientHello.ServerName, cert)
+	return cert, err
+}
+
+func init() {
+	flag.BoolVar(&builtInSSL, "ssl", false,
+		"built-in ssl support. this requires a public IP, and port 80 being available in order to fetch the certs",
+	)
+	flag.BoolVar(&selfSSL, "self-sign", false,
+		"self-signed ssl support. would display a warning on browsers",
+	)
+	flag.IntVar(&port, "port", 13331, "port number")
+}
+
 func main() {
+	flag.Parse()
+
+	stripDomain := regexp.MustCompile(`Domain=[^;]+; `)
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s API - %s\n", r.RemoteAddr, r.URL.String())
-		remote, _ := url.Parse(mux.Vars(r)["url"])
+		varUrl := mux.Vars(r)["url"]
+		pathCleaned := !strings.Contains(varUrl, "://")
+		if pathCleaned {
+			varUrl = strings.ReplaceAll(varUrl, ":/", "://")
+		}
+		remote, _ := url.Parse(varUrl)
 		r.URL.Scheme = remote.Scheme
 		r.URL.Path = remote.Path
 		r.Host = remote.Host
@@ -23,7 +113,13 @@ func main() {
 		proxy := httputil.NewSingleHostReverseProxy(remote)
 		proxy.ModifyResponse = func(resp *http.Response) error {
 			if setCookie := resp.Header.Get("Set-Cookie"); setCookie != "" {
-				setCookie = strings.ReplaceAll(setCookie, "Path=/", "Path=/proxy/"+remote.String()+"/")
+				remotePath := remote.String()
+				if pathCleaned {
+					remotePath = strings.ReplaceAll(remotePath, "://", ":/")
+				}
+				setCookie = strings.ReplaceAll(setCookie, "Path=/", "Path=/proxy/"+remotePath+"/")
+				setCookie = strings.ReplaceAll(setCookie, ";SameSite=None;Secure", "")
+				setCookie = stripDomain.ReplaceAllString(setCookie, "")
 				resp.Header.Set("Set-Cookie", setCookie)
 			}
 			return nil
@@ -47,14 +143,42 @@ func main() {
 	}
 
 	r := mux.NewRouter()
-	r.HandleFunc("/proxy/{url:https?://.*}", handler)
+	r.HandleFunc("/proxy/{url:https?://?.*}", handler)
 	r.PathPrefix("/authenticate").HandlerFunc(fnHandler)
 	r.PathPrefix("/66571939").HandlerFunc(fnHandler)
 	r.PathPrefix("/").HandlerFunc(staticHandler)
 	r.SkipClean(true)
 
-	_ = browser.OpenURL("http://localhost:13331/")
-	log.Printf("Reverse Proxy Server running at :13331\n")
-	http.Handle("/", r)
-	panic(http.ListenAndServe(":13331", r))
+	listenAddr := fmt.Sprintf(":%d", port)
+
+	if builtInSSL {
+		certManager := autocert.Manager{
+			Prompt: autocert.AcceptTOS,
+			Cache:  autocert.DirCache("certs"),
+		}
+
+		server := &http.Server{
+			Addr: listenAddr,
+			TLSConfig: &tls.Config{
+				GetCertificate: certManager.GetCertificate,
+			},
+			Handler: r,
+		}
+		go http.ListenAndServe(":http", certManager.HTTPHandler(nil))
+		log.Fatal(server.ListenAndServeTLS("", ""))
+	} else if selfSSL {
+
+		server := &http.Server{
+			Addr: listenAddr,
+			TLSConfig: &tls.Config{
+				GetCertificate: GetSelfSignedCertificate,
+			},
+			Handler: r,
+		}
+		log.Fatal(server.ListenAndServeTLS("", ""))
+	} else {
+		_ = browser.OpenURL(fmt.Sprintf("http://localhost:%d/", port))
+		log.Println("Reverse Proxy Server running at " + listenAddr + "\n")
+		log.Fatal(http.ListenAndServe(listenAddr, r))
+	}
 }
